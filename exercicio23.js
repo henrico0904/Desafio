b4dae96cd98ff1eab7717e49f0eeee8374d6bc53
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 15:25:02 -0300

    Resolve a atividade 13

---
+++ exercicio23.js
@@ -0,0 +1,17 @@
+/*Organizar lista de candidatos
+A lista de candidatos para entrevista deve ser organizada
+ em ordem alfabética antes de ser enviada ao RH.
+*/
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+//organizar em ordem alfabética o array
+let ordemAlfabetica = candidatos.sort()
+console.table(ordemAlfabetica)
