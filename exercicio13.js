90b5392015c7f6e90e9b53e95c9be8eb1dc73eaf
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 11:43:05 -0300

    Resolve o exercício 13

---
+++ exercicio13.js
@@ -0,0 +1,13 @@
+//Relatório de férias: Primeira semana - Você precisa gerar um relatório com apenas a primeira semana de férias dos colaboradores listados, sem modificar a lista original.
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+]  
+//separando o array sem altera-lo
+console.log(diasFaltas.slice(0,5))
