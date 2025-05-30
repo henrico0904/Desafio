e9df860a09fdbf94ba7823031ad5475ffa75cbc5
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 11:02:05 -0300

    Adiciona novos itens ao array

---
+++ exercicio02.js
@@ -0,0 +1,13 @@
+//Expansão de cardápio - Os itens são: "hambúrguer artesanal" e "wrap de frango"
+let cardapio = [
+    "pizza de calabresa",
+    "lasanha",
+    "sushi",
+    "salada Caesar",
+    "pastel",
+    "yakisoba",
+    "feijoada"
+  ];
+//adiciona itens ao array
+cardapio.push(`hambúrguer artesanal`);
+cardapio.push(`wrap de frango`);
