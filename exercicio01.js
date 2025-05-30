22dd5528e76bfad01c3927c32edc73c20cba5665
Henrico <henrico.ferreira@aluno.senai.br>
2025-05-30 10:58:38 -0300

    Adiciona novos produtos no array

---
+++ exercicio01.js
@@ -0,0 +1,18 @@
+// Cadastro de novos produtos - Atualize o array para incluir os novos produtos "SSD 1TB" e "mesa digitalizadora".
+
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+
+//adicionando os produtos pedidos
+promocoes.push(`SSD 1TB`);
+promocoes.push(`mesa digitalizadora`);
+
+console.table(promocoes)
